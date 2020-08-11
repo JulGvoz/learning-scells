2b1220e278abff9137b691f55694664b4ce2fac6
JulGvoz <julgvoz@tutanota.com>
2020-08-11 13:01:54 +0300

    Remove debugging

--- src/main/scala/FormulaParsers.scala
+++ src/main/scala/FormulaParsers.scala
@@ -11,14 +11,10 @@ object FormulaParsers extends RegexParsers {
       val columnString = "[A-Za-z]+".r findFirstIn s
       columnString match {
         case Some(str) => {
-          println("Parsing " + str)
           val columnStringLength = str.length
-          println("Length: " + columnStringLength)
-          println((columnStringLength until 0 by -1))
           val column = (for ((c, i) <- str zip (columnStringLength until 0 by -1)) yield {
             (c.toUpper - 'A')*math.pow(26, i - 1).toInt
           }).sum
-          println("Resulting value: " + column)
           val row = s.substring(columnStringLength).toInt
           Coord(row, column)
         }
