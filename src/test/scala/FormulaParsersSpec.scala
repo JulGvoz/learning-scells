a1697a842eb05abbac7c8df4836818a0833b5d46
JulGvoz <julgvoz@tutanota.com>
2020-08-11 19:53:59 +0300

    Applied offset to tests

--- src/test/scala/FormulaParsersSpec.scala
+++ src/test/scala/FormulaParsersSpec.scala
@@ -8,7 +8,7 @@ import scells._
 class FormulaParsersSpec extends AnyFlatSpec with should.Matchers {
   "FormulaParsers.cell" should "be able to parse basic cells" in {
     val testCases = List("A1", "A2", "B1", "A0", "z0", "A13") zip 
-                    List(Coord(1, 1), Coord(2, 1), Coord(1, 2), Coord(0, 1), Coord(0, 26), Coord(13, 1))
+                    List(Coord(1, 0), Coord(2, 0), Coord(1, 1), Coord(0, 0), Coord(0, 25), Coord(13, 0))
     
     for (test <- testCases) {
       assert(parseAll(cell, test._1).get == test._2, "(" + test._1 + " == " + test._2.toString + ")")
@@ -17,7 +17,7 @@ class FormulaParsersSpec extends AnyFlatSpec with should.Matchers {
 
   it should "parse cells with longer column values" in {
     val testCases = List("A1", "AA2", "BA1", "AA0", "zA0", "AA13") zip 
-                    List(Coord(1, 1), Coord(2, 27), Coord(1, 53), Coord(0, 27), Coord(0, 677), Coord(13, 27))
+                    List(Coord(1, 0), Coord(2, 26), Coord(1, 52), Coord(0, 26), Coord(0, 676), Coord(13, 26))
     
     for (test <- testCases) {
       assert(parseAll(cell, test._1).get == test._2, "(" + test._1 + " == " + test._2.toString + ")")
