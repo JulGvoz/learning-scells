2acefa0e4a9ba849261bf0c7c6c34d8db421ca8c
JulGvoz <julgvoz@tutanota.com>
2020-08-11 19:05:49 +0300

    Testing multicolumn behaviour

---
+++ src/test/scala/AlphaColumnSpec.scala
@@ -0,0 +1,64 @@
+import org.scalatest._
+import flatspec._
+import matchers.should._
+import org.scalactic.Bool
+
+class AlphaColumnSpec extends AnyFlatSpec with Matchers {
+  import scells.alphacolumn._
+
+  def runTests[A, B](tests: Iterable[A], answers: Iterable[B])(f: A => B): Unit = {
+    for ((test, ans) <- tests zip answers) {
+      assert(f(test) == ans, "(test: " + test + ", correct answer: " + ans + ", given answer: " + f(test) + ")")
+    }
+  }
+
+  val simpleTestRange = 1 to 26
+  val simpleTestAnswers = ('A' to 'Z') map (_.toString)
+
+  val simpleZLess = 1 to 25
+  val simpleZLessAns = ('A' to 'Y') map (_.toString)
+
+  val harderTests = List(   27,   28,   52,   53,   54,   78,  677,  678,  702)
+  val harderAnswers = List("AA", "AB", "AZ", "BA", "BB", "BZ", "ZA", "ZB", "ZZ")
+
+  val harderZLess = List(27, 28, 53, 54)
+  val harderZLessAns = List("AA", "AB", "BA", "BB")
+
+  val hardTests = 19 + 26 * 12 + 26*26 * 7 + 26*26*26 * 10 + 26*26*26*26 * 1 :: // AJGLS
+                  26 + 26 * 26 + 26*26 * 26 + 26*26*26 * 26 + 26*26*26*26 * 26 :: // ZZZZZ
+                  1 + 26 * 1 + 26*26 * 1 + 26*26*26 * 1 + 26*26*26*26 * 1 :: // AAAAA
+                  20 + 26 * 19 + 26*26 * 5 + 26*26*26 * 20 :: Nil // TEST
+  val hardAnswers = List("AJGLS", "ZZZZZ", "AAAAA", "TEST")
+
+  "toAlpha" should "correctly convert ints [1 - 26] to length-1 strings" in {
+    runTests(simpleTestRange, simpleTestAnswers)(_.toAlpha)
+  }
+
+  it should "convert z-less ints [1 - 25] to length-1 strings" in {
+    runTests(simpleZLess, simpleZLessAns)(_.toAlpha)
+  }
+
+  it should "convert ints to length-2 strings" in {
+    runTests(harderTests, hardAnswers)(_.toAlpha)
+  }
+
+  it should "convert z-less ints to length-2 strings" in {
+    runTests(harderZLess, harderZLessAns)(_.toAlpha)
+  }
+
+  it should "convert hard ints to strings" in {
+    runTests(hardTests, hardAnswers)(_.toAlpha)
+  }
+
+  "toColumn" should "convert ints [1 - 26] to length-1 strings" in {
+    runTests(simpleTestAnswers, simpleTestRange)(_.toColumn)
+  }
+
+  it should "convert length-2 strings into ints" in {
+    runTests(harderAnswers, harderTests)(_.toColumn)
+  }
+
+  it should "convert hard strings into ints" in {
+    runTests(hardAnswers, hardTests)(_.toColumn)
+  }
+}
--- src/test/scala/FormulaParsersSpec.scala
+++ src/test/scala/FormulaParsersSpec.scala
@@ -5,10 +5,20 @@ import matchers._
 import scells.FormulaParsers._
 import scells._
 
+@Ignore
 class FormulaParsersSpec extends AnyFlatSpec with should.Matchers {
   "FormulaParsers.cell" should "be able to parse basic cells" in {
     val testCases = List("A1", "A2", "B1", "A0", "z0", "A13") zip 
                     List(Coord(1, 0), Coord(2, 0), Coord(1, 1), Coord(0, 0), Coord(0, 25), Coord(13, 0))
+    
+    for (test <- testCases) {
+      assert(parseAll(cell, test._1).get == test._2, "(" + test._1 + " == " + test._2.toString + ")")
+    }
+  }
+
+  it should "parse cells with longer column values" in {
+    val testCases = List("A1", "AA2", "BA1", "AA0", "zA0", "AA13") zip 
+                    List(Coord(99, 26), Coord(2, 26), Coord(1, 52), Coord(0, 26), Coord(0, 650), Coord(13, 26))
     
     for (test <- testCases) {
       assert(parseAll(cell, test._1).get == test._2, "(" + test._1 + " == " + test._2.toString + ")")
