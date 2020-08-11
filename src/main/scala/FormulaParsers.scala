0e60668ccf29bc3bfbb0388db66ac118a76ffdf7
JulGvoz <julgvoz@tutanota.com>
2020-08-11 20:10:42 +0300

    Finish the rest of the parsers

--- src/main/scala/FormulaParsers.scala
+++ src/main/scala/FormulaParsers.scala
@@ -12,11 +12,37 @@ object FormulaParsers extends RegexParsers {
       val columnString = "[A-Za-z]+".r findFirstIn s
       columnString match {
         case Some(str) => {
-          
           val row = s.substring(str.length).toInt
           Coord(row, str.toUpperCase.toColumn)
         }
         case None => throw new IllegalArgumentException
       }
+    }
+  
+  def range: Parser[Range] = cell~":"~cell ^^ {
+    case c1~":"~c2 => Range(c1, c2)
+  }
+
+  def number: Parser[Number] =
+    decimal ^^ (d => Number(d.toDouble))
+  
+  def application: Parser[Application] = 
+    identifier~"("~repsep(expr, ",")~")" ^^ {
+      case f~"("~ps~")" => Application(f, ps)
+    }
+  
+  def expr: Parser[Formula] = 
+    range | cell | number | application
+  
+  def textual: Parser[Textual] = 
+    """[^=].*""".r ^^ Textual
+  
+  def formula: Parser[Formula] =
+    number | textual | "="~>expr
+  
+  def parse(input: String): Formula = 
+    parseAll(formula, input) match {
+      case Success(result, next) => result
+      case f: NoSuccess => Textual("["+ f.msg + "]")
     }
 }
