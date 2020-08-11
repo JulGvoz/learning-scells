bc342629f621cd63ad11151e4c7d06df1ba14dd0
JulGvoz <julgvoz@tutanota.com>
2020-08-11 19:31:53 +0300

    Rewrite toAlpha, using recursion Now properly handles the edge case of Z

--- src/main/scala/AlphaColumn.scala
+++ src/main/scala/AlphaColumn.scala
@@ -1,40 +1,31 @@
 package scells
 package alphacolumn {
 
-case class AlphaStringWrapper(str: String) {
-  def toColumn: Int = {
-    val length = str.length
-    // get powers like AAB -> 625, 25, 1
-    val powers = (length until 0 by -1) map ((x: Int) => math.pow(26, x - 1).toInt) 
-    val values = for ((char, power) <- str zip powers) yield {
-      (char - 'A' + 1)*power
+  case class AlphaStringWrapper(str: String) {
+    def toColumn: Int = {
+      val length = str.length
+      // get powers like AAB -> 625, 25, 1
+      val powers = (length until 0 by -1) map ((x: Int) => math.pow(26, x - 1).toInt) 
+      val values = for ((char, power) <- str zip powers) yield {
+        (char - 'A' + 1)*power
+      }
+      values.sum
     }
-    values.sum
   }
-}
 
-case class AlphaIntWrapper(col: Int) {
-  def toAlpha: String = {
-    // infinite list of powers: 1, 25, 625, 15625 ...
-    val powers = LazyList.from(0) map ((x: Int) => math.pow(26, x).toInt)
-    // col divided into its digits  
-    val digits = (LazyList.iterate(col)(_ / 26) takeWhile (_ != 0) map (_ % 26)).toList
-    /*
-    On working of digits:
-    Stream.iterate(col)(_ / 26) yields
-    637799, 24530, 943, 36, 1, 0, ...
-    takeWhile (_ != 0) yields
-    637799, 24530, 943, 36, 1
-    map (_ % 26) yields
-    19, 12, 7, 10, 1
-    Thus 58126 describes a 
-    */
-    val reversed = (digits map {
-      (x: Int) => (x + (if (x == 0) 26 else 0) + 'A' - 1).toChar // edge case for Z :/
-    }).mkString
-    reversed.reverse
+  case class AlphaIntWrapper(col: Int) {
+    def toAlpha: String = {
+      def decompose(v: Int): List[Int] = v match {
+        case x if x <= 26 => x :: Nil // its just a single letter, output just it
+        case y if y % 26 == 0 => {
+          26 :: decompose((y - 1) / 26) // special case: Z. since it is mod 26 == 0, it messes up things
+        }
+        case z => z % 26 :: decompose(z / 26) // regular case. just cascade down, like normal decimal/binary/base-n decomposition
+      }
+      (decompose(col) map ((x: Int) => (x + 'A' - 1).toChar))
+      .mkString.reverse
+    }
   }
-}
 
 }
 
