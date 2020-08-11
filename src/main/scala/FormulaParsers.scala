9a057c9f21f47254f24aabef952ae47b9280b776
JulGvoz <julgvoz@tutanota.com>
2020-08-11 13:01:34 +0300

    Fixed bug which failed parsing

--- src/main/scala/FormulaParsers.scala
+++ src/main/scala/FormulaParsers.scala
@@ -11,10 +11,14 @@ object FormulaParsers extends RegexParsers {
       val columnString = "[A-Za-z]+".r findFirstIn s
       columnString match {
         case Some(str) => {
+          println("Parsing " + str)
           val columnStringLength = str.length
-          val column = (for ((c, i) <- str zip (columnStringLength until 1)) yield {
-            (c.toUpper - 'A')*math.pow(26, i).toInt
+          println("Length: " + columnStringLength)
+          println((columnStringLength until 0 by -1))
+          val column = (for ((c, i) <- str zip (columnStringLength until 0 by -1)) yield {
+            (c.toUpper - 'A')*math.pow(26, i - 1).toInt
           }).sum
+          println("Resulting value: " + column)
           val row = s.substring(columnStringLength).toInt
           Coord(row, column)
         }
