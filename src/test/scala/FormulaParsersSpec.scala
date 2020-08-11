4a62e0f13c514552bbaff7f6a63844bf711ad5e7
JulGvoz <julgvoz@tutanota.com>
2020-08-11 19:46:39 +0300

    Fix faulty tests

--- src/test/scala/FormulaParsersSpec.scala
+++ src/test/scala/FormulaParsersSpec.scala
@@ -5,11 +5,10 @@ import matchers._
 import scells.FormulaParsers._
 import scells._
 
-@Ignore
 class FormulaParsersSpec extends AnyFlatSpec with should.Matchers {
   "FormulaParsers.cell" should "be able to parse basic cells" in {
     val testCases = List("A1", "A2", "B1", "A0", "z0", "A13") zip 
-                    List(Coord(1, 0), Coord(2, 0), Coord(1, 1), Coord(0, 0), Coord(0, 25), Coord(13, 0))
+                    List(Coord(1, 1), Coord(2, 1), Coord(1, 2), Coord(0, 1), Coord(0, 26), Coord(13, 1))
     
     for (test <- testCases) {
       assert(parseAll(cell, test._1).get == test._2, "(" + test._1 + " == " + test._2.toString + ")")
@@ -18,7 +17,7 @@ class FormulaParsersSpec extends AnyFlatSpec with should.Matchers {
 
   it should "parse cells with longer column values" in {
     val testCases = List("A1", "AA2", "BA1", "AA0", "zA0", "AA13") zip 
-                    List(Coord(99, 26), Coord(2, 26), Coord(1, 52), Coord(0, 26), Coord(0, 650), Coord(13, 26))
+                    List(Coord(1, 1), Coord(2, 27), Coord(1, 53), Coord(0, 27), Coord(0, 677), Coord(13, 27))
     
     for (test <- testCases) {
       assert(parseAll(cell, test._1).get == test._2, "(" + test._1 + " == " + test._2.toString + ")")
